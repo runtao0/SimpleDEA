ecb44ba404f7cfe2fe17bfc0b4972f65276b1d3c
Runtao0 <ry2228@columbia.edu>
2017-03-01 18:21:02 -0500

    add jest for testing, debugged async call

--- csUtils.js
+++ csUtils.js
@@ -1,14 +1,15 @@
-var csApi = (function  () {
 
-  var api = {},
-      baseUrl = 'https://dea.staging.credsimple.com',
-      path = '/v1/clients_providers/'
-      token = '3ad6aef59ee542ec881c5bc6593ba9c3',
-      id = 20;
+class csApi {
+  constructor(options) {
+     this.baseUrl = 'https://dea.staging.credsimple.com';
+     this.path = '/v1/clients_providers/';
+     this.token = '3ad6aef59ee542ec881c5bc6593ba9c3';
+     this.id = 20;
+  }
 
-  api.getData = function(callback) {
-    requestUrl = baseUrl + path + id + '?token=' + token;
-    request = new XMLHttpRequest();
+  getData(callback) {
+    const requestUrl = this.baseUrl + this.path + this.id + '?token=' + this.token;
+    const request = new XMLHttpRequest();
     request.open('get', requestUrl, true);
     request.onload = function(e) {
       var response = request.response;
@@ -21,6 +22,99 @@ var csApi = (function  () {
     request.send();
   };
 
-  return api;
+};
+// api.getData()
 
-})();
+const simpleDEArequest = {
+  baseUrl: 'https://dea.staging.credsimple.com',
+  path: '/v1/clients_providers/',
+  token: '3ad6aef59ee542ec881c5bc6593ba9c3',
+  id: 20,
+}
+
+
+class csAPI {
+  constructor({ baseUrl, path, token, id }, method = "GET", data = {}) {
+    this.method = method;
+    this.url = baseUrl + path + id + '?token=' + token;
+    this.data = data;
+
+    this._requestPromise = this._requestPromise.bind(this);
+  }
+
+  getData(success, error) {
+    return this._requestPromise().then(success, error);
+  }
+
+  _requestPromise () {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open(this.method, this.url);
+
+      request.onload = event => {
+        if (request.status >= 200 && request.status < 300) {
+          let response = request.response;
+          response = JSON.parse(response);
+          return resolve(response);
+        } else {
+          reject({
+            status: request.status,
+            statusText: request.statusText
+          });
+        }
+      };
+
+      request.onerror = function () {
+        reject({
+          status: request.status,
+          statusText: request.statusText
+        });
+      };
+
+      if (Object.keys(this.data) === 0) {
+        request.send();
+      } else {
+        request.send(JSON.stringify(this.data));
+      }
+    });
+  };
+}
+
+
+// const _requestPromise = ({ method, url, data }) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
+//     request.open(method, url);
+//
+//     request.onload = event => {
+//       if (request.status >= 200 && request.status < 300) {
+//         const response = request.response;
+//         response = JSON.parse(response);
+//         return resolve(response);
+//       } else {
+//         reject({
+//           status: request.status,
+//           statusText: request.statusText
+//         });
+//       }
+//     };
+//
+//     request.onerror = function () {
+//       reject({
+//         status: request.status,
+//         statusText: request.statusText
+//       });
+//     };
+//
+//     request.send(JSON.stringify(data));
+//   });
+// };
+
+// const _extend = function (base, ...objs) {
+//   objs.forEach(obj => {
+//     Object.keys(obj).forEach(key => {
+//       base[key] = obj[key];
+//     });
+//   });
+//   return base;
+// };
