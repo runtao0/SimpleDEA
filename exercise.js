5511f59cd3b57252c48868829fdcb4132737e2fd
Runtao0 <ry2228@columbia.edu>
2017-03-06 12:30:43 -0500

    clean up entry file

--- exercise.js
+++ exercise.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const requestID20 = new csAPI(simpleDEArequest);
   const container = document.getElementById("providers_container");
+  const requestID20 = new csAPI(simpleDEArequest);
   const store = new RecordStore(container);
 
   const setupEventListeners = () => {
@@ -9,15 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const viewButtons = document.getElementById("show_opts");
     store.addEvents(viewButtons, searchBar, pageView);
   }
-  
+
   requestID20.getData(store.receiveRecords, store.showErrorMessage)
-             .then(setupEventListeners);
+             .then(setupEventListeners, store.showErrorMessage);
 });
-
-
-// annotate
-// error message
-// refactor
-// test?
-
-// loading screen
